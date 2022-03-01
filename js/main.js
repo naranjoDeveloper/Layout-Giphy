c83bd8ae1934510839f996b892065ee622542935
naranjo <naranjo.developer@gmail.com>
2022-03-01 09:15:13 -0500

    layout con api de trending integrada

--- js/main.js
+++ js/main.js
@@ -1,21 +1,32 @@
-
+let results = document.getElementById('results');
 
 let getData = async () => {
     try {
         const apiKey = 'VuIHH7Mn8FelC4tUFaaKyVkZD5eQGAIg';
-        const busqueda = 'Cars';
         const url = `https://api.giphy.com/v1/gifs/trending?api_key=${apiKey}`;
         const response = await fetch(url);
         const body = await response.text();
 
         let { data } = JSON.parse(body)
+
+        // console.log(data)
         
-        var d = data.map((x) => x.images.fixed_height.url);
-        
-        d.forEach(url => {
-            let img = document.createElement('img')
-            img.src = url
-            results.appendChild(img)
+        var d = data.map((x) => x);
+
+        console.log(d)
+
+        d.forEach(element => {
+            results.innerHTML += `
+            <div class="col-sm-3">
+                <div class="card my-2">
+                    <img src="${element.images.downsized.url}">  
+                    <div class="card-body">
+                    <h3 class="text-center"> ${element.title} </h3>
+                        <p class="card-text">  </p>
+                    </div>
+                </div>
+            </div>
+            `
         });
 
     } catch (error) {
